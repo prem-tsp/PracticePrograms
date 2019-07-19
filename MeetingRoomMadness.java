b26a67e7b073091aa992143c1d7f81bcde673d9e
prem-tsp <52152981+prem-tsp@users.noreply.github.com>
2019-07-19 12:12:43 +0530

    Java Code

---
+++ MeetingRoomMadness.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class MeetingRoomMadness
+{
+	public static int calculate(int n,int start[],int stop[])
+	{
+		int temp_start=start[0],temp_stop=stop[0],count=1;
+		
+		for(int i=0 ; i<n ; i++)
+		{	
+			if(start[i] > temp_start && temp_stop > start[i])
+			{
+				temp_start = start[i];
+				temp_stop = stop[i];
+				count++;
+			}		
+		}
+		return count;	
+	}
+	public static void main(String[] args)
+	{
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int start[] = new int[n];
+		int stop[] = new int[n];
+		for(int i=0 ; i<n ; i++)
+			start[i] = in.nextInt();
+		for(int i=0 ; i<n ; i++)
+			stop[i] = in.nextInt();
+		int ans = calculate(n,start,stop);
+		System.out.print(ans);
+	}
+}
