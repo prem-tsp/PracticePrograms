e98ee1c7f4f4862a1d1d8609a80ee6b58671148d
prem-tsp <52152981+prem-tsp@users.noreply.github.com>
2019-07-29 12:22:24 +0530

    Java File

---
+++ Product_of_num.java
@@ -0,0 +1,37 @@
+import java.util.*;
+class Product_of_num{
+	public static void product(int arr[],int n)
+	{
+		int temp=1;
+		int ans[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			ans[i]=temp;
+			temp*=arr[i];	
+		}
+               temp=1;
+		for(int i=n-1;i>=0;i--)
+		{
+			ans[i]*=temp;
+			temp*=arr[i];
+		}
+		for(int i=0;i<n;i++)
+		{
+			System.out.print(ans[i]+" ");
+		}
+		
+	}
+
+	public static void main(String[] args)
+	{
+		int n;
+		Scanner in=new Scanner(System.in);
+		n=in.nextInt();
+		int arr[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			arr[i]=in.nextInt();
+		}
+		product(arr,n);
+	}
+}
